ee76fcfc7fa74fa955621c1e419fb027c1be7b43
RemcoG_immalle <remco.goyvaerts@immalle.eu>
2016-11-23 20:24:48 +0100

    Comment Pure menu CSS

--- script/script.js
+++ script/script.js
@@ -6,9 +6,3 @@ var sumbitButton = document.getElementById("sendButton");
 
 sumbitButton.addEventListener("click", submitFunction);
 
-$('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-$('.collapsible').collapsible();
