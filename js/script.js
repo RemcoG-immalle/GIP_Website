2e2ce275ac165a826879b1ef170e68e9665ae58c
RemcoG-immalle <remco.goyvaerts@immalle.be>
2016-11-24 15:40:43 +0100

    Working materialize menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+$('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 240
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+}
+);
+
 function submitFunction() {
     alert("Deze button verstuurd nog geen contact formulier. Moest ik deze pagina laten staan, zal ik dit wel in orde brengen.");
 }
