a29c1e4f09339b79d702bf8263af355cc124e68b
RemcoG-immalle <remco.goyvaerts@immalle.be>
2016-11-24 14:58:43 +0100

    Clean links

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function submitFunction() {
+    alert("Deze button verstuurd nog geen contact formulier. Moest ik deze pagina laten staan, zal ik dit wel in orde brengen.");
+}
+
+var sumbitButton = document.getElementById("sendButton");
+
+sumbitButton.addEventListener("click", submitFunction);
