0e8f1bf361089c6d333112aa624fc8fc8aefca7c
RemcoG_immalle <remco.goyvaerts@immalle.eu>
2016-12-02 19:39:51 +0100

    Base FAQ

--- js/script.js
+++ js/script.js
@@ -26,8 +26,23 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-      $('.slider').slider({full_width: true});
+  $('.slider').slider({full_width: true});
 });
+
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
+
+$('.modal').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    in_duration: 300, // Transition in duration
+    out_duration: 200, // Transition out duration
+    starting_top: '4%', // Starting top style attribute
+    ending_top: '10%', // Ending top style attribute
+  }
+);
 
 // Pie Charts
 
